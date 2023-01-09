7d28c8f39cb69eaf9cfb5db7b386c33bef2186e1
Jacob Ràfols <ovnijacob@gmail.com>
2023-01-09 23:36:21 +0100

    copy admin from last project

--- backend/apps/core/admin.py
+++ backend/apps/core/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
 
+from .models import *
+from apps.vampire.models import VampireClan, VampireCC, HumanCC
+
 # Register your models here.
+class RelationshipsInline(admin.TabularInline):
+    model = CharacterRelationship
+    fk_name = "character1"
+
+
+class VampireCCInline(admin.StackedInline):
+    model = VampireCC
+    fk_name = "character"
+
+class HumanCCInline(admin.StackedInline):
+    model = HumanCC
+
+class CharacterAdmin(admin.ModelAdmin):
+    list_display = ["name", "pc"]
+    list_filter = ["pc"]
+    inlines = [RelationshipsInline, HumanCCInline, VampireCCInline]
+
+
+
+class PlotStageInline(admin.StackedInline):
+    model = PlotStages
+
+class PlotAdmin(admin.ModelAdmin):
+    inlines = [PlotStageInline]
+
+
+
+class EventInline(admin.StackedInline):
+    model = Event
+
+class SesionAdmin(admin.ModelAdmin):
+    inlines = [EventInline]
+
+
+admin.site.register(Chronicle)
+admin.site.register(VampireClan)
+admin.site.register(Character, CharacterAdmin)
+admin.site.register(Location)
+admin.site.register(Plot, PlotAdmin)
+admin.site.register(Sesion, SesionAdmin)
