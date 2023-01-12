a06a54023b24fef7e4a84e13b06e69d0a584c227
jeroqui <ovnijacob@gmail.com>
2023-01-12 23:59:49 +0100

    decision about relay

--- backend/src/modules/apps/core/schema/query.py
+++ backend/src/modules/apps/core/schema/query.py
@@ -4,14 +4,22 @@ from graphene_django.filter import DjangoFilterConnectionField
 from src.modules.utils.schema import CustomNode
 from . import types as coretypes
 
-from src.modules.apps.core.models import Character
+from src.modules.apps.core.models import Character, Chronicle, CharacterRelationship
 
 class Query(graphene.ObjectType):
-    character = CustomNode.Field(coretypes.CharacterType)
-    chronicle_characters = DjangoFilterConnectionField(coretypes.CharacterType)
+    chronicles = graphene.List(coretypes.ChronicleType)
+    character = graphene.Field(coretypes.CharacterType)
+    chronicle_characters = graphene.List(coretypes.CharacterType, chronicle=graphene.Int(required=True))
+
+    def resolve_chronicles(self, info):
+        return Chronicle.objects.all()
 
     def resolve_character(self, info, id):
         return Character.objects.get(pk=id)
 
-    def reolve_chronicle_characters(self, info, chronicle_id):
-        return Character.objects.filter(chronicle=chronicle_id)
+    def resolve_chronicle_characters(self, info, chronicle):
+        chronicle = Chronicle.objects.filter(pk=chronicle).first()
+        if not chronicle:
+            return Character.objects.none()
+
+        return Character.objects.filter(chronicle=chronicle)
--- backend/src/modules/apps/core/schema/types.py
+++ backend/src/modules/apps/core/schema/types.py
@@ -1,21 +1,29 @@
 import graphene
 from graphene_django import DjangoObjectType, DjangoListField 
 
-from src.modules.utils.schema import CustomNode
-from ..models import Character, CharacterRelationship
 
+from src.modules.utils.schema import CustomNode
+from ..models import Character, CharacterRelationship, Chronicle
+
+
+class ChronicleType(DjangoObjectType):
+    class Meta:
+        model = Chronicle
+        filter_fields = ['name']
+        fields = ("id", "name")
+        interfaces = ()
 
 class CharacterRelationshipType(DjangoObjectType):
     class Meta:
         model = CharacterRelationship
-
+        fields = ("id", "feeling", "character2")
 
 class CharacterType(DjangoObjectType):
     class Meta:
         model = Character
         filter_fields = ['chronicle']
         fields = ("id", "chronicle", "name", "story", "pc")
-        interfaces = (CustomNode, )
+        interfaces = ()
 
     relationships = DjangoListField(CharacterRelationshipType)
 
