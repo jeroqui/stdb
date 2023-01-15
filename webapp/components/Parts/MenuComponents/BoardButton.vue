5f85c8d8a49e8b1d3f6f68f5c5f96c74dc78b044
jeroqui <ovnijacob@gmail.com>
2023-01-15 16:40:38 +0100

    menu buttons transitions

--- webapp/components/Parts/MenuComponents/BoardButton.vue
+++ webapp/components/Parts/MenuComponents/BoardButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex hover:bg-dark">
+    <div class="flex bg-black hover:bg-dark transition-colors">
         <div class="w-16 shrink-0 grow-0">
             <div class="rounded-full flex justify-center">
                 <slot name="icon"></slot>
--- webapp/components/Parts/Panel.vue
+++ webapp/components/Parts/Panel.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="max-w-3xl mx-auto my-10">
+    <div class="max-w-3xl mx-auto my-10 transition-all duration-75">
         <StyleGlass>
             <div class="p-10">
                 <slot></slot>
