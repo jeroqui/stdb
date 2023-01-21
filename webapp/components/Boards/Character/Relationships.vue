50b0e9c47835544fc532fce5b24171bcd019b809
jeroqui <ovnijacob@gmail.com>
2023-01-21 17:19:21 +0100

    relationships component mockup

--- webapp/components/Boards/Character/Relationships.vue
+++ webapp/components/Boards/Character/Relationships.vue
@@ -1,5 +1,18 @@
 <template>
     <div class="my-10">
         <h2 class="text-2xl">Relationships</h2>
+
+        <div>
+            <div v-for="i in [1, 2, 3]"
+                class="grid grid-cols-3"
+            >
+                <div>Character name</div>
+                <div class="text-center">-- <i>loves</i> --></div>
+                <div class="text-right">character {{ i }}</div>
+            </div>
+            <button>
+                Add
+            </button>
+        </div>
     </div>
 </template>
