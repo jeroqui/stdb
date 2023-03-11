994b23dd59020e634cfdc5b0ad805668dbf719ce
Jacob Ràfols <ovnijacob@gmail.com>
2023-03-11 23:22:42 +0100

    fixed build error on migrate

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -13,7 +13,7 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_DB'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'db',
-        'PORT': '5432',
+        'HOST': 'database',
+        'PORT': 5432,
     }
 }
